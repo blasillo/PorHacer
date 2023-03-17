1ab032fb1c76cf07c265ee959c32af31219628c3
blas <btorregrosa@infor.uva.es>
2023-03-17 08:57:25 +0100

    Inicio

---
+++ src/main/java/es/jcyl/cursofswd/porhacer/PorHacerApplication.java
@@ -0,0 +1,13 @@
+package es.jcyl.cursofswd.porhacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorHacerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PorHacerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/jcyl/cursofswd/porhacer/PorHacerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jcyl.cursofswd.porhacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorHacerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
