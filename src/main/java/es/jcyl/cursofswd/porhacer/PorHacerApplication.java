da85c15737ccf5b3082a43c26d0a99aa2bbae76c
blas <btorregrosa@infor.uva.es>
2023-03-18 12:55:51 +0100

    Mejora

--- src/main/java/es/jcyl/cursofswd/porhacer/PorHacerApplication.java
+++ src/main/java/es/jcyl/cursofswd/porhacer/PorHacerApplication.java
@@ -2,12 +2,12 @@ package es.jcyl.cursofswd.porhacer;
 
 import es.jcyl.cursofswd.porhacer.modelos.PorHacer;
 import es.jcyl.cursofswd.porhacer.repositorios.PorHacerRepositorio;
-import jdk.nashorn.internal.runtime.logging.Logger;
 import lombok.extern.log4j.Log4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class PorHacerApplication {
--- src/main/java/es/jcyl/cursofswd/porhacer/config/AppConfig.java
+++ src/main/java/es/jcyl/cursofswd/porhacer/config/AppConfig.java
@@ -1,7 +1,10 @@
 package es.jcyl.cursofswd.porhacer.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+
 }
